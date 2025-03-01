b2ec5bb333399949b3b4685af809bffb10a6a23c
jirapat-mol <jirapat.mo@hotmail.com>
2025-03-02 04:11:57 +0700

    init

---
+++ streamlit_python.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import kagglehub
+import pandas as pd
+# path = kagglehub.dataset_download("laotse/credit-risk-dataset")
+# print("Path to dataset files:", path)
+df = pd.read_csv("credit_risk_dataset.csv")
+
+
+# st.title('Streamlit Example')
