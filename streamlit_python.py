704a0b6c0362df527861695af380ccee4eb662e8
jirapat-mol <jirapat.mo@hotmail.com>
2025-03-02 05:18:03 +0700

    init

--- streamlit_python.py
+++ streamlit_python.py
@@ -1,8 +1,19 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
 # path = kagglehub.dataset_download("laotse/credit-risk-dataset")
 # print("Path to dataset files:", path)
 df = pd.read_csv("credit_risk_dataset.csv")
 
+st.set_page_config(
+    page_title="Credit Risk Analysis",
+    page_icon="🧊",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 
-# st.title('Streamlit Example')
+st.sidebar.title("Credit Risk Analysis")
+st.title("Risk Analysis and Portfolio Management")
+st.subheader("NgernTurbo")
+
+st.table(df.head(20).style.highlight_max(subset=['person_income'], color='pink'))
